394b055931578c08290528358597d382626c73d4
GuillaumeTrck <guillaumetricknot@gmail.com>
2021-12-27 14:35:12 +0100

    FAIT CHIER

---
+++ src/main/java/com/projet3bsi/projet3bsi/Models/UnitPeople.java
@@ -0,0 +1,36 @@
+package com.projet3bsi.projet3bsi.Models;
+
+import java.util.HashSet;
+
+public class UnitPeople {
+    private HashSet<String> unitPeople;
+
+    /* Constructor */
+    public UnitPeople (String currentUnit){
+        unitPeople = new HashSet<>();
+        switch (currentUnit)
+        {
+            case "Pédiatrie":
+                unitPeople.add("Guillaume Tricknot");
+                unitPeople.add("Maëva Casu");
+                unitPeople.add("Theo Desoil");
+                unitPeople.add("Victor Fatoux");
+                break;
+            case "Urgences":
+                unitPeople.add("Banjamin Tricknot");
+                unitPeople.add("Laurent Casu");
+                unitPeople.add("Robin Van Roy");
+                break;
+            case "Oncologie":
+                unitPeople.add("Walter Tricknot");
+                unitPeople.add("Gaëtan Czornomudiak");
+                break;
+        }
+
+    }
+
+    /* methods */
+    public HashSet<String> getUnitPeople(){
+        return unitPeople;
+    }
+}
