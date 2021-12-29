6b791ea3de5c8bee3def7bab7cb5831d4e337504
Desoil Théo <theodesoil@gmail.com>
2021-12-29 22:44:03 +0100

    Ajout des données pour chaque patients

--- src/main/java/com/projet3bsi/projet3bsi/Models/Patients.java
+++ src/main/java/com/projet3bsi/projet3bsi/Models/Patients.java
@@ -11,10 +11,49 @@ public class Patients {
         switch (currentPatient)
         {
             case "Guillaume Tricknot":
-                patients.add("Age : 19 ans");
+                patients.add("Adresse : chaussé de Binche 221, 7000 Mons");
+                patients.add("Date de naissance : 18/01/2002");
+
                 break;
             case "Maëva Casu":
-                patients.add("Age : 20 ans");
+                patients.add("Adresse : Avenue de la rue 21, 7000 Ghlin");
+                patients.add("Date de naissance : 22/11/1998");
+
+                break;
+            case "Theo Desoil" :
+                patients.add("Adresse : Rue du clavier 23, 7340 Paturageq");
+                patients.add("Date de naissance : 08/08/2010");
+
+                break;
+            case "Victor Fatoux" :
+                patients.add("Adresse : chassée de l'écran 16, 1679 hornu");
+                patients.add("Date de naissance : 26/09/1976");
+
+                break;
+            case "Banjamin Tricknot" :
+                patients.add("Adresse : Avenue de la souris 76, 2754 Dour");
+                patients.add("Date de naissance : 30/11/1987");
+
+                break;
+            case "Laurent Casu" :
+                patients.add("Adresse : Rue des hauts parleurs 245, 7664 Charleroi");
+                patients.add("Date de naissance : 09/12/1968");
+
+                break;
+            case "Robin Van Roy" :
+                patients.add("Adresse : chaussé de la caméra 167, 5286 Saint-Ghislain");
+                patients.add("Date de naissance : 17/07/2015");
+
+                break;
+            case "Walter Tricknot" :
+                patients.add("Adresse : Avenue des touches 67, 5649 Binche");
+                patients.add("Date de naissance : 12/03/2018");
+
+                break;
+            case "Gaëtan Czornomudiak" :
+                patients.add("Adresse : Rue de l'alimentation 298, 2389 Boussu");
+                patients.add("Date de naissance : 29/06/1991");
+
                 break;
         }
 
