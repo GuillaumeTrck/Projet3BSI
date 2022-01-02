c324b4c39d38ff2db6e88e272d00f3b6b6f9c234
Desoil Théo <theodesoil@gmail.com>
2022-01-02 11:55:45 +0100

    Login + code et userfiled ok

--- src/main/java/com/projet3bsi/projet3bsi/Controlers/LoginController.java
+++ src/main/java/com/projet3bsi/projet3bsi/Controlers/LoginController.java
@@ -4,6 +4,7 @@ import com.projet3bsi.projet3bsi.CareApplication;
 import com.projet3bsi.projet3bsi.Models.Patients;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -21,9 +22,16 @@ import javafx.stage.Window;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Objects;
 
 
 public class LoginController {
+    static String pwdFieldR = "20";
+    static String userFieldR = "une note sur 20";
+    @FXML
+    private TextField userField;
+    @FXML
+    private PasswordField pwdField;
 
 
     public void BtnReturnClick(ActionEvent actionEvent) {
@@ -41,34 +49,15 @@ public class LoginController {
         mainWindow.setScene(scene); //here we simply set the new scene
     }
 
+
     public static class Main extends Application {
 
         @Override
         public void start(Stage primaryStage) throws Exception{
-            Text scenetitle = new Text("Welcome");
-            Label userName = new Label( "User Name:" );
-            TextField userTextField = new TextField();
-            Label pw = new Label("Password:");
-            PasswordField pwBox = new PasswordField();
-            Button btn = new Button( "Sign in" );
-
-            VBox vb = new VBox();
-            vb.getChildren().add(scenetitle);
-            GridPane gp = new GridPane();
-            gp.add(userName,0,0);
-            gp.add(userTextField,1,0);
-            gp.add(pw,0,1);
-            gp.add(pwBox,1,1);
-            vb.getChildren().add(gp);
-            vb.getChildren().add(btn);
-
-            scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-            vb.setSpacing(10);
-            vb.setPadding(new Insets(10));
-            gp.setHgap(10);
-            gp.setVgap(10);
-
-
+            FXMLLoader fxmlLoginController = new FXMLLoader(CareApplication.class.getResource("login.fxml"));
+            primaryStage.setTitle("Login");
+            primaryStage.setScene(new Scene(fxmlLoginController.load(),300,275));
+            primaryStage.show();
 
 
         }
@@ -76,22 +65,27 @@ public class LoginController {
     }
 
     public void OnButonClick(ActionEvent actionEvent) {
-        FXMLLoader fxmlUnitPeople = new FXMLLoader(CareApplication.class.getResource("unit-people-view.fxml"));
+        if(userField.getText().equals(userFieldR) && pwdField.getText().equals(pwdFieldR)){
+            FXMLLoader fxmlUnitPeople = new FXMLLoader(CareApplication.class.getResource("unit-people-view.fxml"));
 
-        Scene scene = null;
-        try {
-            scene = new Scene(fxmlUnitPeople.load(), 320, 240);
-        } catch (IOException e) {
-            e.printStackTrace();
+            Scene scene = null;
+            try {
+                scene = new Scene(fxmlUnitPeople.load(), 320, 240);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // get controller associated with the view
+            UnitPatientsController unitPatientsController = fxmlUnitPeople.getController();
+            // set the current unit care
+            unitPatientsController.setListView(currentUnitCare);
+
+            Stage mainWindow; //Here is the magic. We get the reference to main Stage.
+            mainWindow = (Stage) Window.getWindows().get(0);
+            mainWindow.setScene(scene); //here we simply set the new scene
         }
-        // get controller associated with the view
-        UnitPatientsController unitPatientsController = fxmlUnitPeople.getController();
-        // set the current unit care
-        unitPatientsController.setListView(currentUnitCare);
-
-        Stage mainWindow; //Here is the magic. We get the reference to main Stage.
-        mainWindow = (Stage) Window.getWindows().get(0);
-        mainWindow.setScene(scene); //here we simply set the new scene
+        else {
+            System.out.println("Identifiant ou mot de passe incorrect !");
+        }
     }
 
     private static String currentUnitCare;
